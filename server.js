5692f156cb13493dd504df62ceb5b20fc0d837b8
= <lokesh960077152@gmail.com>
2020-12-10 20:06:52 +0530

    db

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/Mypockt" , {useNewUrlParser : true , useUnifiedTopology:true});
 const bodyParser = require('body-parser');
 var ejs = require('ejs');
-var x  = String(Math.floor(Math.random()*101))
-console.log(x)
+//database section
+var accountSchema =  new mongoose.Schema({
+    
+    goal: String,
+    finalAmount:Number,
+    InitialAmount:Number,
+    Note:String
+    
+});
+
+var maindb = new mongoose.Schema({
+    firstname :String,
+    lastname : String,
+    email : String,
+    password : String,
+    Account:[accountSchema]
+});
+var main = mongoose.model("main",maindb);
+var account = mongoose.model("account",accountSchema)
+
+const one = new account({
+    goal:"ghghhgh",
+    finalAmount:12000,
+    InitialAmount:0,
+    Note:"gjggf"
+})
+one.save()
+
+
+//end of database section
 
 const app = express();
 app.set('view engine', 'ejs');
 const port = 3000;
-arr = [];
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+'/public'));
 app.get("/", (req,res)=>{
@@ -16,33 +45,24 @@ app.get("/", (req,res)=>{
 })
 
 
-
-
-
-
 // authentication of the account
 app.post("/", (req,res)=>{
     let emailx = req.body.email;
     let pass = req.body.password;
-    console.log(emailx,pass)
-    for(var i = 0 ; i < arr.length ; i++){
-        if(arr[i].email === emailx){
-            if(arr[i].password === pass){
-                console.log("entry accepted");
-                app.get("/main" , (req,res)=>{
-                    res.render("main.ejs",{})
-                })
-                res.redirect("/main");
-            }
-            else{
-               console.log("password wrong")
+    main.findOne({email:emailx},function(err,save){
+        if(err){
+            console.error(err);
+        }else{
+            if(save){
+                if(save.password === pass){
+                    app.get("/main",(req,res)=>{
+                        res.render("main")
+                    })
+                }
+                res.redirect("/main")
             }
         }
-        else{
-        console.log("email wrong ")
-        }
-        
-    } 
+    })
 });
 app.get("/sign-up" , (req,res)=>{
     res.sendFile(__dirname+"/"+"signup-page.html");
@@ -50,17 +70,29 @@ app.get("/sign-up" , (req,res)=>{
 })
 // generation of the account 
 app.post("/sign-up" , (req,res)=>{
- let x = { 
+ main.create({
     firstname : req.body.firstName,
     lastname : req.body.lastName,
     email : req.body.email,
     password : req.body.password
- };
- console.log("account registered")
- console.log(x)
- arr.push(x)
-res.redirect("/");
-})
+ }, function(err,save){
+     if(err){
+         console.error(err)
+     }else{
+         console.log(save)
+         main.updateOne({email:"lokesh960077152@gmail.com"},{Account:one},function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log("successfully updated");
+            }
+        });
+         res.redirect("/")
+     }
+ });
+ 
+});
+ 
 app.listen(process.env.PORT || port, ()=>{
     console.log("server is running");
-})
+});
