dbbab39210641290cb86ed18b3ed03cb15432f37
= <lokesh960077152@gmail.com>
2020-12-12 10:45:31 +0530

    done

--- server.js
+++ server.js
@@ -3,37 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/Mypockt" , {useNewUrlParser : true , useUnifiedTopology:true});
 const bodyParser = require('body-parser');
 var ejs = require('ejs');
-//database section
-var accountSchema =  new mongoose.Schema({
-    
-    goal: String,
-    finalAmount:Number,
-    InitialAmount:Number,
-    Note:String
-    
-});
-
-var maindb = new mongoose.Schema({
-    firstname :String,
-    lastname : String,
-    email : String,
-    password : String,
-    Account:[accountSchema]
-});
-var main = mongoose.model("main",maindb);
-var account = mongoose.model("account",accountSchema)
-
-const one = new account({
-    goal:"ghghhgh",
-    finalAmount:12000,
-    InitialAmount:0,
-    Note:"gjggf"
-})
-one.save()
-
-
-//end of database section
-
 const app = express();
 app.set('view engine', 'ejs');
 const port = 3000;
@@ -42,8 +11,28 @@ app.use(express.static(__dirname+'/public'));
 app.get("/", (req,res)=>{
     res.sendFile(__dirname+"/"+"homepage.html");
 
-})
+});
+app.get("/sign-up" , (req,res)=>{
+    res.sendFile(__dirname+"/"+"signup-page.html");
+    
+});
 
+// generation of the account 
+app.post("/sign-up" , (req,res)=>{
+    main.create({
+       firstname : req.body.firstName,
+       lastname : req.body.lastName,
+       email : req.body.email,
+       password : req.body.password
+    }, function(err,save){
+        if(err){
+            console.error(err)
+        }else{
+            console.log(save)
+            res.redirect("/")
+        }
+    });
+   });
 
 // authentication of the account
 app.post("/", (req,res)=>{
@@ -64,35 +53,49 @@ app.post("/", (req,res)=>{
         }
     })
 });
-app.get("/sign-up" , (req,res)=>{
-    res.sendFile(__dirname+"/"+"signup-page.html");
-    
+
+app.post("/main",(req,res)=>{
+    let x =  {
+        "goal" : req.body.goal,
+        "note" : req.body.note,
+        "final": req.body.final,
+        "initial": req.body.initial
+    }
+    console.log(x)
+    res.redirect("/main")
+
 })
-// generation of the account 
-app.post("/sign-up" , (req,res)=>{
- main.create({
-    firstname : req.body.firstName,
-    lastname : req.body.lastName,
-    email : req.body.email,
-    password : req.body.password
- }, function(err,save){
-     if(err){
-         console.error(err)
-     }else{
-         console.log(save)
-         main.updateOne({email:"lokesh960077152@gmail.com"},{Account:one},function(err){
-            if(err){
-                console.log(err);
-            }else{
-                console.log("successfully updated");
-            }
-        });
-         res.redirect("/")
-     }
- });
- 
+//database section
+var accountSchema =  new mongoose.Schema({
+    
+    goal: String,
+    finalAmount:Number,
+    InitialAmount:Number,
+    Note:String
+    
 });
- 
+
+var maindb = new mongoose.Schema({
+    firstname :String,
+    lastname : String,
+    email : String,
+    password : String,
+    Account:[accountSchema]
+});
+var main = mongoose.model("main",maindb);
+var account = mongoose.model("account",accountSchema)
+
+// const one = new account({
+//     goal:"ghghhgh",
+//     finalAmount:12000,
+//     InitialAmount:0,
+//     Note:"gjggf"
+// })
+// one.save()
+
+
+//end of database section
+
 app.listen(process.env.PORT || port, ()=>{
     console.log("server is running");
 });
